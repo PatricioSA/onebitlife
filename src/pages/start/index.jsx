1bab09db010c35c457e543c6d73ed391c0849174
Patricio Samuel <patricioanjosjf@gmail.com>
2022-11-29 15:46:32 -0300

    a

--- src/pages/start/index.jsx
+++ src/pages/start/index.jsx
@@ -4,7 +4,7 @@ import { View, Text, Image, ScrollView } from 'react-native';
 
 export default function Start() {
     return (
-        <View>
+        <View style={styles.container}>
             <ScrollView showVerticalScrollIndicator={false}>
                 <View>
                     <Image source={require('../../assets/icons/logo3.png')}></Image>
@@ -17,3 +17,17 @@ export default function Start() {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundcolor: "rgba(21, 21, 21 0.98)"
+    },
+
+    logo: {
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20,
+    },
+});
