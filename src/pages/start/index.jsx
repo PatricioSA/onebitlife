d005856fbe659348913139a889dbeeee1cc7d9cc
Patricio Samuel <patricioanjosjf@gmail.com>
2022-11-29 16:06:34 -0300

    Estilização da página Start

--- src/pages/start/index.jsx
+++ src/pages/start/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
+import { View, Text, Image, ScrollView, StyleSheet } from 'react-native';
 
 
 export default function Start() {
     return (
         <View style={styles.container}>
             <ScrollView showVerticalScrollIndicator={false}>
-                <View>
-                    <Image source={require('../../assets/icons/logo3.png')}></Image>
-                    <Text>
+                <View style={{alignItems: 'center'}}>
+                    <Image source={require('../../assets/icons/logo3.png')}
+                        style={styles.logo}
+                    />
+                    <Text style={styles.description}>
                         Vamos transformar sua vida {'\n'} em jogo, buscando sempre {'\n'}
                         o melhor nivel.
                     </Text>
@@ -21,7 +23,7 @@ export default function Start() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundcolor: "rgba(21, 21, 21 0.98)"
+        backgroundColor: "rgba(21, 21, 21, 0.98)"
     },
 
     logo: {
@@ -30,4 +32,11 @@ const styles = StyleSheet.create({
         marginTop: 60,
         marginBottom: 20,
     },
+
+    description: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        textAlign: 'center',
+        marginVertical: 60
+    }
 });
