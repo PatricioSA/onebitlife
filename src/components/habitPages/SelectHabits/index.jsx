0f7e5564edb73a253225dd5c2c7dac3437d5cad8
Patricio Samuel <patricioanjosjf@gmail.com>
2022-12-14 14:30:38 -0300

    Criação dos componentes (SelectHabit)

---
+++ src/components/habitPages/SelectHabits/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+import { SelectList } from "react-native-dropdown-select-list";
+
+export default function SelectHabit({habit, habitInput}) {
+    return(
+        <>
+        
+        </>
+    );
+};
