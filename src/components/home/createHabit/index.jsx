c071592baf1179be582af39173891c6bbb205b65
Patricio Samuel <patricioanjosjf@gmail.com>
2022-12-14 14:21:52 -0300

    Estruturando itens por navegação

--- src/components/home/createHabit/index.jsx
+++ src/components/home/createHabit/index.jsx
@@ -1,16 +1,22 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
-export default function CreateHabit({habitArea, borderColor}) {
+export default function CreateHabit({ habitArea, borderColor }) {
+
+    const navigation = useNavigation();
 
     function handleCreate() {
-        console.log(`Botão clicado da área: ${habitArea}`);
+        navigation.navigate('HabitPage', {
+            create: true,
+            habit: { habitArea: habitArea },
+        });
     }
 
     return (
         <TouchableOpacity
             activeOpacity={0.8}
-            style={[styles.button, {borderColor: borderColor}]}
+            style={[styles.button, { borderColor: borderColor }]}
             onPress={handleCreate}
         >
             <Text style={styles.habitTitle}>
--- src/pages/habitPages/index.jsx
+++ src/pages/habitPages/index.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Alert } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 
-export default function HabitPage() {
+export default function HabitPage({route}) {
 
     const navigation = useNavigation();
+
+    const {create, habit} = route.params;
 
     return (
         <View style={styles.container}>
@@ -21,7 +23,11 @@ export default function HabitPage() {
                         />
                     </TouchableOpacity>
                     <View style={styles.mainContent}>
-
+                        <Text style={styles.title}>Configurações {'\n'} de hábito</Text>
+                        <Text style={styles.inputText}>Área</Text>
+                        <View style={styles.inputContainer}>
+                            <Text style={styles.area}>{habit?.habitArea}</Text>
+                        </View>
                     </View>
                 </View>
             </ScrollView>
@@ -50,4 +56,32 @@ const styles = StyleSheet.create({
         width: 250,
         alignSelf: 'center',
     },
+
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 30,
+    },
+
+    inputText: {
+        color: 'white',
+        fontSize: 16,
+        marginTop: 35,
+        marginBottom: 10,
+        marginLeft: 5,
+    },
+
+    inputContainer: {
+        borderWidth: 1,
+        borderColor: '#FFFFFF',
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+    },
+
+    area: {
+        color: '#BBBBBB',
+        fontSize: 15,        
+    }
 });
